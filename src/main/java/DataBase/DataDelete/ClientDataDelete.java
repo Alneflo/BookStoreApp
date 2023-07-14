42022e9636f08db72333a67a60db9ce65b0b6447
Alneflo <alejandro.nebot@cuatroochenta.com>
2023-07-14 16:33:56 +0200

    Add functionability to Data Delete classes

--- src/main/java/DataBase/DataDelete/ClientDataDelete.java
+++ src/main/java/DataBase/DataDelete/ClientDataDelete.java
@@ -1,5 +1,26 @@
 package DataBase.DataDelete;
 
-public class ClientDataDelete {
+import java.sql.Connection;
+import java.sql.Statement;
 
+import Data.Client;
+import DataBase.SQLiteConnection;
+
+public class ClientDataDelete {
+	
+	private static Connection con = new SQLiteConnection().getConnection();
+	private static Statement stmt;
+	private static String sql;
+	
+	public static void deleteData(Client c) {
+		try {
+			stmt = con.createStatement();
+			
+			sql = ("REMOVE FROM client WHERE dni = " + c.getDni() + ";");
+			
+			stmt.executeUpdate(sql);
+		}catch(Exception e) {
+			System.err.println("Couldn't delete data");
+		}
+	}
 }
